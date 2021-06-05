3745aadea244e327e37d188bcdeb099de4c314f7
ss2261 <ss2261@kent.ac.uk>
2021-06-05 12:53:47 +0100

    login form aligned directly in the center of the page

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,15 +1,34 @@
 <template>
   <v-container>
-<h1>test</h1>
-</v-container>
+    <div style="position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);">
+
+      <v-card :loading="loading" class="mx-auto my-12" style="width:500px;">
+        <h1 style="font-family: Verdana">Login</h1>
+
+        <v-divider class="mx-4"></v-divider>
+
+        <v-card-title>Username</v-card-title>
+
+        <v-card-title>Password</v-card-title>
+
+        <v-card-actions>
+          <v-btn color="deep-purple lighten-2" text @click="reserve">
+            Reserve
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'login',
-  props: {
-  }
-}
+  name: "login",
+  props: {},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
